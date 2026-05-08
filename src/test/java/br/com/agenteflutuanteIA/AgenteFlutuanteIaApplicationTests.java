e4d7dbc5985ca92ad1643bd67f4416805aafd723
Jean Heberth Souza Vieira <jeanheberth19@gmail.com>
2026-05-08 16:41:08 -0300

    Initial commit

---
+++ src/test/java/br/com/agenteflutuanteIA/AgenteFlutuanteIaApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.agenteflutuanteIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteFlutuanteIaApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
+
